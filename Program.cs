eebcaf052269d8e63109890a30a4ada0bab4ee7e
dev1ce2004 <ktesij00@fesb.hr>
2026-03-23 18:01:25 +0100

    git changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Pogram
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
